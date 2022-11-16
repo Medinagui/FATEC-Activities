63a49ce4b881c171ad07c0dc88a6c26dd02582d9
Guilherme Medina <gui.castro1906@gmail.com>
2022-11-16 17:34:49 -0300

    Classe pilha criada em Ex1 - Trab2 - Estrutura de dados

--- Estrutura de Dados/Trabalho 2/Ex1.cpp
+++ Estrutura de Dados/Trabalho 2/Ex1.cpp
@@ -13,9 +13,66 @@ Caso 2 e 4: Remover e Imprimir Item: Verificação antes da chamada da função 
 
 using namespace std;
 
+class Pilha{
+    
+    private:
+        float *itens;
+        int topo, tam_max;
+
+    public:
+        Pilha(int n) 
+        {
+            itens = new float[n];
+            tam_max = n;
+            topo = 0;
+        }
+    
+    bool empty()
+    {
+        return (topo == 0);
+    }
+
+    bool full()
+    {
+        return (topo == tam_max);
+    }
+
+    int size()
+    {
+        return topo;
+    }
+
+    void push(float valor) 
+    {
+        if (full())
+        {
+            cout << "\n\n- PILHA CHEIA. ITEM NÃO ADICIONADO - \n\n";
+            return;
+        }
+
+        itens[topo] = valor;
+        topo++;
+    }
+
+    void pop()
+    {
+        if(empty())
+        {
+            cout << "\n\n- ATENÇÃO, PILHA VAZIA -\n\n";
+            return;
+        }
+        topo--;
+    }
+
+    float top()
+    {
+        if(!empty()) return itens[topo - 1];
+    }
+};
+
+
 void imprimeMenu()
 {
-    system("cls");
     cout << "PROGRAMA PILHA\n\n";
     cout << "[ 0 ] - Sair";
     cout << "\n[ 1 ] - Inserir Itens";
@@ -26,7 +83,6 @@ void imprimeMenu()
 
 int main(int argc, char const *argv[])
 {
-    imprimeMenu();
     system("pause");
     return 0;
 }
